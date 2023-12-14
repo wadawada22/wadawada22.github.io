532285b7f9a160c27dafa4cf82e8515ff4d7345b
wadawada22 <117604073+wadawada22@users.noreply.github.com>
2023-12-14 19:37:23 +0200

    Add files via upload

--- lab6_save.php
+++ lab6_save.php
@@ -1,4 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
     $data = $_POST;
     $json_data = json_encode($data);
     file_put_contents('accordion_data.json', $json_data);
